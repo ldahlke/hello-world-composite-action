09b671d2cbf951c20fa5c518b4703c9db90ce421
Lars Dahlke <lars.dahlke@gmx.de>
2021-09-30 15:55:43 +0200

    More debug

--- goodbye.sh
+++ goodbye.sh
@@ -2,11 +2,11 @@ echo "The environment is:"
 echo
 env
 pwd
-cd $(RUNNER_WORKSPACE)
+cd $RUNNER_WORKSPACE
 cd work
 pwd
 ls -la
-git clone $(GITHUB_SERVER_URL)/ldahlke/schulcloud-server
+git clone $GITHUB_SERVER_URL/ldahlke/schulcloud-server
 ls
 ls schulcloud-server
 echo
