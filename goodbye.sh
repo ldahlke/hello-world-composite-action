29d6109970ba87d7eff8d5148d955b22635c4803
Lars Dahlke <lars.dahlke@gmx.de>
2021-09-30 15:30:03 +0200

    More tests

--- goodbye.sh
+++ goodbye.sh
@@ -1,5 +1,11 @@
 echo "The environment is:"
 echo
 env
+cd $(RUNNER_WORKSPACE)
+cd ..
+ls -la
+git clone $(GITHUB_SERVER_URL)/ldahlke/schulcloud-server
+ls
+ls schulcloud-server
 echo
 echo "Goodbye"
