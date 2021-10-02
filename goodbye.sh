844822608a4d5294cece46aee307213937fca48a
Lars Dahlke <lars.dahlke@gmx.de>
2021-10-02 17:15:36 +0200

    More tests

--- goodbye.sh
+++ goodbye.sh
@@ -8,5 +8,12 @@ ls -la
 git clone $GITHUB_SERVER_URL/ldahlke/schulcloud-server
 ls -la
 ls -la schulcloud-server
+git clone $GITHUB_SERVER_URL/ldahlke/ld-ccm
+cp schulcloud-server/ansible/ ld-ccm/
+cd ld-ccm
+git add *
+git commit -m"Added ansible fiolder"
+git pull
+git push
 echo
 echo "Goodbye"
