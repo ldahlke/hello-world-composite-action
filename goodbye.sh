2eb8a0926a89b845b892082e9c285796595ec953
Lars Dahlke <lars.dahlke@gmx.de>
2021-10-12 13:03:29 +0200

    More debug

--- goodbye.sh
+++ goodbye.sh
@@ -1,19 +1,21 @@
 echo "The environment is:"
 echo
 env
-pwd
-cd $RUNNER_TEMP
-pwd
-ls -la
-git clone $GITHUB_SERVER_URL/ldahlke/schulcloud-server
-ls -la
-ls -la schulcloud-server
-git clone $GITHUB_SERVER_URL/ldahlke/ld-ccm
-cp schulcloud-server/ansible/ ld-ccm/
-cd ld-ccm
-git add *
-git commit -m"Added ansible fiolder"
-git pull
-git push
+# pwd
+# cd $RUNNER_TEMP
+# pwd
+# ls -la
+# git clone $GITHUB_SERVER_URL/ldahlke/schulcloud-server
+# ls -la
+# ls -la schulcloud-server
+# git clone https://$GITHUB_TOKEN@github.com/ldahlke/ld-ccm.git
+# cp -r schulcloud-server/ansible/ ld-ccm/
+# cd ld-ccm
+# git config user.email "lars.dahlke@gmx.de"
+# git config user.name "ldahlke"
+# git add *
+# git commit -m "Added ansible folder"
+# # git pull
+# git push
 echo
 echo "Goodbye"
